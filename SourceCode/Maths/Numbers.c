d865bf0a8989602aa2485b16eecb2b9b1747462d
Milind Jain <jainmilind311@gmail.com>
2020-04-30 16:49:53 +0530

    OPTIMIZED IN MASTER NUMBER.C

--- SourceCode/Maths/Numbers.c
+++ SourceCode/Maths/Numbers.c
@@ -2,11 +2,16 @@
 #include<math.h>
 
 int sum(int x, int y){
-  return x+y;
+	//Changingg!!
+      	return x+y;
 }
+
 int sub(int x, int y){
-return x-y;
+	//Following change made in the master branch
+	int result = x-y;
+	return result;
 }
+
 
 //FOLLOWING ARE MADE IN TESTING BRANCH
 	int mul(int x, int y){
