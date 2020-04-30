a65525d49b22bdc0a3fa7560b254ba23726cfbee
Milind Jain <jainmilind311@gmail.com>
2020-04-30 11:42:34 +0530

    Added functionality for Greeting 	Added String.cpp 		Added Function greeting(char []) in String.cpp 	Updated Hello.c 		Added Few more Messages

--- SourceCode/Hello.c
+++ SourceCode/Hello.c
@@ -2,4 +2,5 @@
 
 int main(){
 	printf("Hello World");
+	Printf("SSSSSSSSSSSSSSSSS8up!!!!!!1")
 }
---
+++ SourceCode/String.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	cout<<"This is C++"<<endl;
+	cout<<"We'll be learning java next"<<endl
+}
