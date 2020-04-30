9915948bc184de93baa595c3ccc619a4d5ec778f
Milind Jain <jainmilind311@gmail.com>
2020-04-30 16:46:24 +0530

    Modified Numbers.c in bugFix Optimized SUM function

--- SourceCode/Maths/Numbers.c
+++ SourceCode/Maths/Numbers.c
@@ -2,14 +2,17 @@
 #include<math.h>
 
 int sum(int x, int y){
-  return x+y;
+    //FOLLOWING CHANGE MADE IN BUGFIX
+    int result = x+y;
+    return result;
+
 }
 int sub(int x, int y){
-return x-y;
+	return x-y;
 }
 
 //FOLLOWING ARE MADE IN TESTING BRANCH
 	int mul(int x, int y){
 		return x*y;
 	}
-
+// END
